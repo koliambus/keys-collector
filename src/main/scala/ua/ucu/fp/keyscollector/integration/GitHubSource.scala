5155947106805b70e442825e8bb279f94ba7c27e
dobosevych <dobosevych@gmail.com>
2021-01-14 00:42:27 +0200

    Improved code readability

--- src/main/scala/ua/ucu/fp/keyscollector/integration/GitHubSource.scala
+++ src/main/scala/ua/ucu/fp/keyscollector/integration/GitHubSource.scala
@@ -21,21 +21,23 @@ object GitHubSource {
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
   val SEARCH_URI: Uri = "https://api.github.com/search/code"
 
+  def request(source: Map[String, String]): Future[List[KeyFinding]] = {
+    val mapper = new ObjectMapper() with ScalaObjectMapper
+    mapper.registerModule(DefaultScalaModule)
+
+    Http().singleRequest(
+        HttpRequest(uri = SEARCH_URI.withQuery(Query("q" -> source("q"), "access_token" -> Config.GITHUB_TOKEN)))
+          .addHeader(RawHeader("Accept", "application/vnd.github.v3.text-match+json")))
+        .map(x => x.entity)
+        .flatMap(x => Unmarshal(x).to[String])
+        .map(x => mapper.readValue[Map[String, Any]](x))
+        .map(x => x("items").asInstanceOf[List[Object]].map(RepositoryDecoder(source("service"), _)))
+
+  }
   def apply(params: List[Map[String, String]]): Source[KeyFinding, Cancellable] = Source
     .tick(1.second, 10.second, "tick")
     .mapAsync(1) { _ =>
-      val mapper = new ObjectMapper() with ScalaObjectMapper
-      mapper.registerModule(DefaultScalaModule)
-      val responseFuture: Future[List[List[KeyFinding]]] = Future.sequence(params.map(x => {
-        Http().singleRequest(
-          HttpRequest(uri = SEARCH_URI.withQuery(Query("q" -> x("q"), "access_token" -> Config.GITHUB_TOKEN)))
-            .addHeader(RawHeader("Accept", "application/vnd.github.v3.text-match+json")))
-      }.map(x => x.entity)
-        .flatMap(x => Unmarshal(x).to[String])
-        .map(x => mapper.readValue[Map[String, Any]](x))
-        .map(y => y("items").asInstanceOf[List[Object]].map(RepositoryDecoder(x("service"), _)))
-      ))
-      responseFuture
+      Future.sequence(params.map(request))
     }
     .mapConcat(x => x)
     .mapConcat(x => x)
